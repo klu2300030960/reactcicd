4aae67118b8e98cf4e40eb16dbb7afc9d27267ef
klu2300030960 <2300030960@kluniversity.in>
2025-08-12 17:17:56 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import App from './App.jsx'
+
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/reactcicd/', // Must match your repo name
   plugins: [react()],
-  base:'/reactcicd/',
 })
